27bba84146959167e184b464ed584399a5f807a3
Nathan Wilk <nwilk01@manhattan.edu>
2017-02-23 00:26:52 -0500

    Stuff still does not work

--- Android/PillCentral/app/src/main/java/android/pillcentral/Login.java
+++ Android/PillCentral/app/src/main/java/android/pillcentral/Login.java
@@ -1,113 +1,179 @@
 package android.pillcentral;
 
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.HideReturnsTransformationMethod;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.app.ProgressDialog;
 import android.widget.Toast;
+import android.util.Log;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.client.methods.HttpGet;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.ArrayList;
+import app.AppConfig;
+import app.AppController;
+import helper.SQLiteHandler;
+import helper.SessionManager;
+
 
 public class Login extends AppCompatActivity {
-    private EditText editusername;
-    private EditText editpassword;
+    private static final String TAG = Login.class.getSimpleName();
+    private EditText inputusername;
+    private EditText inputpassword;
+    private Button btnLogin;
+    private ProgressDialog pDialog;
+    private SessionManager session;
+    private SQLiteHandler db;
 
-    public static final String USER_NAME = "USERNAME";
 
-    String username;
-    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        editusername = (EditText) findViewById(R.id.editText);
-        editpassword = (EditText) findViewById(R.id.editText2);
-    }
 
+        //register button and input boxes
+        inputusername = (EditText) findViewById(R.id.editText);
+        inputpassword = (EditText) findViewById(R.id.editText2);
+        btnLogin = (Button) findViewById(R.id.button);
 
-    protected void onclick(View view) {
-        String pass = editpassword.getText().toString();
-        String user = editusername.getText().toString();
+        //Progress Dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
 
-        login(user, pass);
+        //SQLite Database
+        db = new SQLiteHandler((getApplicationContext()));
 
-    }
+        //Session Manager
+        session = new SessionManager(getApplicationContext());
 
-    private String login(String Username, String Password) {
-        class LoginAsync extends AsyncTask<String, Void, String> {
+        if (session.isLoggedIn()) {
+            Intent intent = new Intent(Login.this, pilldisplay.class);
+            startActivity(intent);
+            finish();
+        }
 
-            protected void onPreExecute(){
-            }
-            @Override
-            protected String doInBackground(String... arg0) {
-                try {
-                    String username = (String) arg0[0];
-                    String password = (String) arg0[1];
-                    String link = "http://turing.manhattan.edu/~nwilk01/GetDatabasePassword.php?username=" + username;
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String username = inputusername.getText().toString();
+                String password = inputpassword.getText().toString();
 
-                    URL url = new URL(link);
-                    HttpClient client = new DefaultHttpClient();
-                    HttpGet request = new HttpGet();
-                    request.setURI(new URI(link));
-                    HttpResponse response = client.execute(request);
-                    BufferedReader in = new BufferedReader(new
-                            InputStreamReader(response.getEntity().getContent()));
-
-                    StringBuffer sb = new StringBuffer("");
-                    String line = "";
-
-                    while ((line = in.readLine()) != null) {
-                        sb.append(line);
-                        break;
-                    }
-
-                    in.close();
-                    return sb.toString();
-
-
-                } catch (Exception e) {
-                    return new String("Exception: " + e.getMessage());
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    checkLogin(username, password);
+                } else if (username.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter username", Toast.LENGTH_LONG).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter username & password", Toast.LENGTH_LONG).show();
                 }
             }
+
+        });
+    }
+
+    private void checkLogin(final String username, final String password)
+    {
+        final String tag_string_req = "req_login";
+
+        pDialog.setMessage("Logging in ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Method.POST, AppConfig.URL_LOGIN, new Response.Listener<String>()
+        {
+
+            @Override
+            public void onResponse(String response)
+            {
+                Log.d(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try{
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    //checks error node in json
+                    if(!error)
+                    {
+                        //successful login
+                        session.setLogin(true);
+
+                        //store user in SQLite
+                        JSONObject user = jObj.getJSONObject("user");
+                        String uname = user.getString("name");
+                        String pass = user.getString("password");
+                        String email = user.getString("email");
+
+                        db.addUser(uname, pass,email);
+
+                        //Launch pilldisplay
+                        Intent intent = new Intent(Login.this, pilldisplay.class);
+                        startActivity(intent);
+                        finish();
+                    }else
+                    {
+                        String errorMsg = jObj.get("error_msg").toString();
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                }catch (JSONException e)
+                {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+    }){
+        @Override
+            protected Map<String, String> getParams()
+        {
+            //Posting paramters to login URL
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("username", username);
+            params.put("password", password);
+
+            return params;
         }
-        return result;
+        };
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void showDialog()
+    {
+        if(!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog()
+    {
+        if(pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
+
+
+
 
 
 
---
+++ Android/PillCentral/app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by Admin on 2/22/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://turing.manhattan.edu/~nwilk01/Android/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://turing.manhattan.edu/~nwilk01/register.php";
+}
